91771d35c790549e16b85057d15a9797a0e86b0a
tarsoly <tarsoly@yahoo.com>
2016-02-09 10:48:16 +0100

    some cleanup and improvement: Data has different color from header. Data is inline with header. Data (remote name, fsaa, ip) is updated when changes are made.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 400, height: 768});
+  mainWindow = new BrowserWindow({width: 400, height: 740});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');

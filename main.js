f12ee8fc945a65e82183db3820ea3d286f83af63
tarsoly <tarsoly@yahoo.com>
2016-02-09 13:48:03 +0100

    error message if valid VirCA version is not found some appearance update

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 400, height: 740});
+  mainWindow = new BrowserWindow({width: 420, height: 750});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
